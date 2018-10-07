ce2a3c57d7b0f54b96508bded78df81e0a047db7
raghunathv <kvraghu.129@gmail.com>
2018-10-07 17:56:06 +0200

    compile passes

--- lab3/src/sparkstreaming/KafkaSpark.scala
+++ lab3/src/sparkstreaming/KafkaSpark.scala
@@ -25,22 +25,26 @@ object KafkaSpark {
     val session = cluster.connect()
     session.execute("CREATE KEYSPACE IF NOT EXISTS avg_space WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };")
     session.execute("CREATE TABLE IF NOT EXISTS avg_space.avg (word text PRIMARY KEY, count float);")
+    
 
+    //val sparkConf = new SparkConf().setAppName("DirectKafkaWordCount")
+    val sparkConf = new SparkConf().setAppName("DirectKafkaAvg").setMaster("local[2]").set("spark.executor.memory","1g");
+    val ssc = new StreamingContext(sparkConf, Seconds(2))
 
     // make a connection to Kafka and read (key, value) pairs from it
-    val kafkaStream = KafkaUtils.createStream(streamingContext,
-[ZK quorum], [consumer group id], [per-topic number of Kafka partitions to consume])
     val kafkaConf = Map(
             "metadata.broker.list" -> "localhost:9092",
             "zookeeper.connect" -> "localhost:2181",
             "group.id" -> "kafka-spark-streaming",
             "zookeeper.connection.timeout.ms" -> "1000")
-    val messages = KafkaUtils.createDirectStream.[[key class], [value class], [key decoder class], [value decoder class]](
-streamingContext, [map of Kafka parameters], [set of topics to consume])
+    val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaConf, Set("avg"))
     
+    val values = messages.map( _._2)
+    val pairs = values.flatMap{ _.split(" ") }.map(x => (x, 1)).reduceByKey(_ + _)
+    //val pairs =values.flatMap{ _.split(" ") }.map(i => { var s=i.split(",") ( s(0), s(1).toInt) })
 
     // measure the average value for each key in a stateful manner
-    def mappingFunc(key: String, value: Option[Double], state: State[Double]): (String, Double) = {
+    def mappingFunc(key: String, value: Option[Int], state: State[(Double,Long)]): Option[(String, Double)] = {
 	val (prevSum,prevN)= state.getOption.getOrElse((0D,0L))
         val (sum,n) = ((prevSum +value.getOrElse(0) ).toDouble , prevN +1L)
                             
