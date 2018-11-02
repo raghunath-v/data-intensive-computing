4891b94b5dd1eef18e2c85302c3547737d73037d
raghunathv <kvraghu.129@gmail.com>
2018-11-02 17:46:20 +0100

    new solution

--- project/run.sh
+++ project/run.sh
@@ -41,41 +41,46 @@ Data="$(hdfs dfs -cat output/part-r-00000 | sort -n -k2 -r | head -n5 | sed -e '
 
 # Write script.js
 cat >./script.js <<EOF
-	var ctx = document.getElementById("myChart").getContext('2d');
-	var myChart = new Chart(ctx, {
-	    type: 'bar',
-	    data: {
-	        labels: [$Labels],  // ["Red", "Blue", "Yellow", "Green", "Purple"],
-	        datasets: [{
-	            label: '# of views per video',
-	            data: [$Data],  // [12, 19, 3, 5, 2],
-	            backgroundColor: [
-	                'rgba(255, 99, 132, 0.2)',
-	                'rgba(54, 162, 235, 0.2)',
-	                'rgba(255, 206, 86, 0.2)',
-	                'rgba(75, 192, 192, 0.2)',
-	                'rgba(153, 102, 255, 0.2)',
-	            ],
-	            borderColor: [
-	                'rgba(255,99,132,1)',
-	                'rgba(54, 162, 235, 1)',
-	                'rgba(255, 206, 86, 1)',
-	                'rgba(75, 192, 192, 1)',
-	                'rgba(153, 102, 255, 1)',
-	            ],
-	            borderWidth: 1
-	        }]
-	    },
-	    options: {
-		maintainAspectRatio: false,
-	        scales: {
-	            yAxes: [{
-	                ticks: {
-	                    beginAtZero:true
-	                }
-	            }]
-	        }
-	    }
-	});
+var ctx = document.getElementById("myChart").getContext('2d');
+var colorList = [];
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: [$Labels],  // ["Red", "Blue", "Yellow", "Green", "Purple"],
+        datasets: [{
+            label: '# of views per video',
+            data: [$Data],  // [12, 19, 3, 5, 2],
+            backgroundColor: colorList,
+            borderColor: colorList,
+            borderWidth: 1
+        }]
+    },
+    options: {
+	maintainAspectRatio: false,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+for (i = 0; i < myChart.data.datasets[0].data.length; i++) {
+        pointBackgroundColors.push(getRandomColor());
+}
+
+myChart.update();
+
 EOF
 fi
